0ff553aecb732e87889edf31f1d137e35c2e26e8
Pei <pei_zh@icloud.com>
2019-12-02 20:12:38 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -9,40 +9,42 @@ data_train_a1, data_val_a1, data_test_a1 = a1.data_preprocessing()
 data_train_a2, data_val_a2, data_test_a2 = a2.data_preprocessing()
 data_train_b1, data_val_b1, data_test_b1 = b1.data_preprocessing()
 data_train_b2, data_val_b2, data_test_b2 = b2.data_preprocessing()
-print('All data loaded.')
+print('All data prepared.')
 
 # ======================================================================================================================
 # Task A1
+print('Task A1 starts.')
 model_A1 = a1.A1()
 acc_A1_train = model_A1.train(data_train_a1, data_val_a1)
 acc_A1_test = model_A1.test(data_test_a1)
-print('Task A1 completed.')
+print('Task A1 ends.')
 del model_A1, data_train_a1, data_val_a1, data_test_a1
 
 # ======================================================================================================================
 # Task A2
+print('Task A2 starts.')
 model_A2 = a2.A2()
-acc_A2_train = model_A2.train(data_train_a2, data_val_a2,
-                              load_model=True)  # setting load_model=False takes 30 minutes to train a CNN from scratch,
-                                                 # whereas setting load_model=True loads a pre-trained model.
+acc_A2_train = model_A2.train(data_train_a2, data_val_a2)
 acc_A2_test = model_A2.test(data_test_a2)
-print('Task A2 completed.')
+print('Task A2 ends.')
 del model_A2, data_train_a2, data_val_a2, data_test_a2
 
 # ======================================================================================================================
 # Task B1
+print('Task B1 starts.')
 model_B1 = b1.B1()
 acc_B1_train = model_B1.train(data_train_b1, data_val_b1)
 acc_B1_test = model_B1.test(data_test_b1)
-print('Task B1 completed.')
+print('Task B1 ends.')
 del model_B1, data_train_b1, data_val_b1, data_test_b1
 
 # ======================================================================================================================
 # Task B2
+print('Task B2 starts.')
 model_B2 = b2.B2()
 acc_B2_train = model_B2.train(data_train_b2, data_val_b2)
 acc_B2_test = model_B2.test(data_test_b2)
-print('Task B2 completed.')
+print('Task B2 ends.')
 del model_B2, data_train_b2, data_val_b2, data_test_b2
 
 # ======================================================================================================================
